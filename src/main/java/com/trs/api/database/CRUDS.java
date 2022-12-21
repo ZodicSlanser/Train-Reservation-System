d9d0449d665ae95bccd12aa7e58b3fb1732c9240
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-12-21 22:21:33 +0200

    Created basic utility CRUD file

---
+++ src/main/java/com/trs/api/database/CRUDS.java
@@ -0,0 +1,120 @@
+package com.trs.api.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class CRUDS {
+    private static ResultSet RS;
+    private static PreparedStatement PS;
+    private static boolean CONNECTED = false;
+    private static Connection connection;
+
+
+    //Connect to the database at instantiation
+    static {
+        try {
+            connection = ConnectionManager.initDatabaseConnection();
+            System.out.println("Connection Successful");
+            CONNECTED = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Failed to connect to the database.");
+        }
+    }
+
+    /*** Utility Methods ***/
+
+    //Check if connection was made successfully or not
+    protected static boolean isConnected() {
+        return CONNECTED;
+    }
+
+    //Utility method to return all values in a table
+    protected static ResultSet getAll(String table) throws SQLException {
+        if (isConnected()) {
+            PS = connection.prepareStatement("SELECT * FROM " + table);
+            RS = PS.executeQuery();
+            return RS;
+        }
+        return null;
+    }
+
+    //Utility method to return all values in a table with a condition
+    protected static ResultSet getAll(String table, String condition) throws SQLException {
+        if (isConnected()) {
+            PS = connection.prepareStatement("SELECT * FROM " + table + " WHERE " + condition);
+            RS = PS.executeQuery();
+            return RS;
+        }
+        return null;
+    }
+
+    //Utility method to return a single value from a table
+    protected static ResultSet get(String table, String column, String condition) throws SQLException {
+        if (isConnected()) {
+            PS = connection.prepareStatement("SELECT " + column + " FROM " + table + " WHERE " + condition);
+            RS = PS.executeQuery();
+            return RS;
+        }
+        return null;
+    }
+
+    //Utility method to insert values into a table
+    protected static void insert(String tableName, List<String> columns, List<String> values) throws SQLException {
+        if (isConnected()) {
+            String sql = "INSERT INTO " + tableName + " (" + String.join(", ", columns) + ") VALUES (" + String.join(", ", values) + ")";
+            System.out.println(sql);
+            PS = connection.prepareStatement(sql);
+            PS.executeUpdate();
+        }
+    }
+
+    //Utility method to update values in a table where a condition is met
+    protected static void update(String tableName, List<String> columns, List<String> values, String condition) throws SQLException {
+        if (isConnected()) {
+            String sql = "UPDATE " + tableName + " SET " + String.join(", ", columns) + " = " + String.join(", ", values) + " WHERE " + condition;
+            System.out.println(sql);
+            PS = connection.prepareStatement(sql);
+            PS.executeUpdate();
+        }
+    }
+
+    //Utility method to update a single in a table where a condition is met
+    protected static void update(String tableName, String columns, String values, String condition) throws SQLException {
+        if (isConnected()) {
+            String sql = "UPDATE " + tableName + " SET " + String.join(", ", columns) + " = " + String.join(", ", values) + " WHERE " + condition;
+            System.out.println(sql);
+            PS = connection.prepareStatement(sql);
+            PS.executeUpdate();
+        }
+    }
+
+    //Utility method to delete everything from the database
+    protected static void prune(String tableName) {
+        String sql = "DELETE FROM " + tableName + " WHERE " + " 1=1 ";
+    }
+
+    //a utility method to delete a row from a table where a condition is met
+    protected static void deleteWhereEqual(String tableName, String column, String value) throws SQLException {
+        if (isConnected()) {
+            String sql = "DELETE FROM " + tableName + " WHERE " + column + " = " + value;
+            System.out.println(sql);
+            PS = connection.prepareStatement(sql);
+            PS.executeUpdate();
+        }
+    }
+
+    protected static void deleteWhereLessThan(String tableName, String column, String value) throws SQLException {
+        if (isConnected()) {
+            String sql = "DELETE FROM " + tableName + " WHERE " + column + " < " + value;
+            System.out.println(sql);
+            PS = connection.prepareStatement(sql);
+            PS.executeUpdate();
+        }
+    }
+
+
+}
