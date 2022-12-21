706d4d306532c8333832b6a48dbd3da8e3f6165a
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-12-21 22:34:41 +0200

    Fixed the structure and introduced new variables **Added new columns for more data ***add a trainType and departure and arrival stations and times

--- src/main/java/com/trs/modules/Train.java
+++ src/main/java/com/trs/modules/Train.java
@@ -2,50 +2,94 @@ package com.trs.modules;
 
 import com.trs.modules.tickets.Ticket;
 
-public class Train {
-    String number;
-    String name;
-    int maximumCapacity;
-    int currentCapacity;
+import java.sql.Timestamp;
 
-    Train(String number, String name, int maximumCapacity){
-        this.number = number;
-        this.name = name;
+
+public class Train {
+    private int trainNumber;
+    private String type;
+    private Timestamp departureTime;
+    private Timestamp arrivalTime;
+    private String departureStation;
+    private String arrivalStation;
+    private int maximumCapacity;
+    private int currentCapacity;
+
+    public Train(int trainNumber, String type, Timestamp departureTime, Timestamp arrivalTime, String departureStation, String arrivalStation, int maximumCapacity) {
+        this.trainNumber = trainNumber;
+        this.type = type;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.departureStation = departureStation;
+        this.arrivalStation = arrivalStation;
         this.maximumCapacity = maximumCapacity;
     }
-    public String getName() {
-        return name;
+
+    public int getTrainNumber() {
+        return trainNumber;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public void setTrainNumber(int trainNumber) {
+        this.trainNumber = trainNumber;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDepartureStation() {
+        return departureStation;
+    }
+
+    public void setDepartureStation(String departureStation) {
+        this.departureStation = departureStation;
+    }
+
+    public String getArrivalStation() {
+        return arrivalStation;
+    }
+
+    public void setArrivalStation(String arrivalStation) {
+        this.arrivalStation = arrivalStation;
+    }
+
+    public Timestamp getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Timestamp departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Timestamp getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Timestamp arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
     public int getMaximumCapacity() {
         return maximumCapacity;
     }
+
     public void setMaximumCapacity(int maximumCapacity) {
         this.maximumCapacity = maximumCapacity;
     }
-    public int getCurrentCapacity() {
-        return currentCapacity;
-    }
-    public void setCurrentCapacity(int currentCapacity) {
-        this.currentCapacity = currentCapacity;
-    }
-    public String getNumber() {
-        return number;
-    }
-    public void setNumber(String number) {
-        this.number = number;
-    }
 
     //TODO: add Implementation
-    public void addTicket(Ticket ticket){
-        if(currentCapacity < maximumCapacity){
+    public void addTicket(Ticket ticket) {
+        if (currentCapacity < maximumCapacity) {
             currentCapacity++;
         }
     }
-    public void removeTicket(Ticket ticket){
-        if(currentCapacity > 0){
+
+    public void removeTicket(Ticket ticket) {
+        if (currentCapacity > 0) {
             currentCapacity--;
         }
     }
