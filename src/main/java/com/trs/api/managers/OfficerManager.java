0995ebde9426f71b09f494ecd819f4cf5eeb6ab2
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-12-22 16:37:23 +0200

    Updated the implementations of getTicketingOfficer to allow it to reflect the type of the officer as in the database

--- src/main/java/com/trs/api/managers/OfficerManager.java
+++ src/main/java/com/trs/api/managers/OfficerManager.java
@@ -1,6 +1,6 @@
 package com.trs.api.managers;
 
-import com.trs.api.database.CRUDS;
+import com.trs.api.database.CRUD;
 import com.trs.modules.SystemAdmin;
 import com.trs.modules.TicketingOfficer;
 
@@ -9,10 +9,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OfficerManager extends CRUDS {
-    private final static List<String> OFFICER_COLUMNS = List.of("AID", "salary", "FirstName", "LastName", "PhoneNumber", "Address");
+public class OfficerManager extends CRUD {
+    private final static List<String> OFFICER_COLUMNS = List.of("AID", "salary", "FName", "LName", "PhoneNumber", "Address");
 
-    OfficerManager() {
+    public OfficerManager() {
         super();
     }
 
@@ -28,6 +28,16 @@ public class OfficerManager extends CRUDS {
         );
     }
 
+    //a method that deletes everything in the table
+    public static void deleteAll() throws SQLException {
+        if (isConnected()) {
+            prune("officers");
+            System.out.println("Everything Deleted");
+            return;
+        }
+        throw new SQLException("Connection to the database failed.");
+    }
+
     //a method that returns all TicketingOfficer from database
     public List<TicketingOfficer> getAllTicketingOfficers() throws SQLException {
         List<TicketingOfficer> ticketingOfficers = new ArrayList<>();
@@ -35,24 +45,34 @@ public class OfficerManager extends CRUDS {
         while (rs.next()) {
             switch (rs.getInt("Type")) {
                 case 1 ->
-                        ticketingOfficers.add(new TicketingOfficer(rs.getInt("AID"), rs.getString("FirstName"), rs.getString("LastName"), rs.getString("PhoneNumber"), rs.getInt("salary"), rs.getString("Address")));
+                        ticketingOfficers.add(new TicketingOfficer(rs.getInt("AID"), rs.getString("FName"), rs.getString("LName"), rs.getString("PhoneNumber"), rs.getInt("salary"), rs.getString("Address")));
                 case 0 ->
-                        ticketingOfficers.add(new SystemAdmin(rs.getInt("AID"), rs.getString("FirstName"), rs.getString("LastName"), rs.getString("PhoneNumber"), rs.getInt("salary"), rs.getString("Address")));
+                        ticketingOfficers.add(new SystemAdmin(rs.getInt("AID"), rs.getString("FName"), rs.getString("LName"), rs.getString("PhoneNumber"), rs.getInt("salary"), rs.getString("Address")));
             }
         }
         return ticketingOfficers;
     }
 
     //a method that returns a single TicketingOfficer from database
-    public TicketingOfficer getTicketingOfficer(String condition) throws SQLException {
-        ResultSet rs = get("officers", "*", condition);
+    public TicketingOfficer getTicketingOfficer(int AID) throws SQLException {
+        ResultSet rs = get("officers", "AID", String.valueOf(AID));
         if (rs.next()) {
             switch (rs.getInt("Type")) {
                 case 1 -> {
-                    return new TicketingOfficer(rs.getInt("AID"), rs.getString("FirstName"), rs.getString("LastName"), rs.getString("PhoneNumber"), rs.getInt("salary"), rs.getString("Address"));
+                    return new TicketingOfficer(rs.getInt("AID"),
+                            rs.getString("FName"),
+                            rs.getString("LName"),
+                            rs.getString("PhoneNumber"),
+                            rs.getInt("salary"),
+                            rs.getString("Address"));
                 }
                 case 0 -> {
-                    return new SystemAdmin(rs.getInt("AID"), rs.getString("FirstName"), rs.getString("LastName"), rs.getString("PhoneNumber"), rs.getInt("salary"), rs.getString("Address"));
+                    return new SystemAdmin(rs.getInt("AID"),
+                            rs.getString("FName"),
+                            rs.getString("LName"),
+                            rs.getString("PhoneNumber"),
+                            rs.getInt("salary"),
+                            rs.getString("Address"));
                 }
             }
         }
@@ -63,23 +83,33 @@ public class OfficerManager extends CRUDS {
     public void insertTicketingOfficer(TicketingOfficer ticketingOfficer) throws SQLException {
         if (isConnected()) {
             insert("officers", OFFICER_COLUMNS, getOfficerValues(ticketingOfficer));
+            return;
         }
-        throw new SQLException("Connection to the database failed.");
+        throw new SQLException("Not connected to database");
+
     }
 
     //a method that updates a TicketingOfficer in the database
-    public void updateTicketingOfficer(TicketingOfficer ticketingOfficer, String condition) throws SQLException {
+    public void updateTicketingOfficer(TicketingOfficer ticketingOfficer) throws SQLException {
         if (isConnected()) {
-            update("officers", OFFICER_COLUMNS, getOfficerValues(ticketingOfficer), condition);
+            try {
+                update("officers", OFFICER_COLUMNS, getOfficerValues(ticketingOfficer), "AID = " + ticketingOfficer.getId());
+                return;
+            } catch (SQLException e) {
+                throw new SQLException("Ticketing Officer doesn't exists");
+            }
         }
         throw new SQLException("Connection to the database failed.");
     }
 
     //a method that deletes a TicketingOfficer from the database
-    public void deleteTicketingOfficer(String Column, String condition) throws SQLException {
+    public void deleteTicketingOfficer(TicketingOfficer officer) throws SQLException {
         if (isConnected()) {
-            deleteWhereEqual("officers", "*", condition);
+            deleteWhereEqual("officers", "AID", String.valueOf(officer.getId()));
+            return;
         }
         throw new SQLException("Connection to the database failed.");
     }
+
+
 }
