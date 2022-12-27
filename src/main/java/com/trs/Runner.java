83547494074cc36612d45c6f98c73ca7eb412efd
ZodicSlanser <zodic.slanser68@yahoo.com>
2022-12-27 08:35:38 +0200

    prepared the login as the first page

--- src/main/java/com/trs/Runner.java
+++ src/main/java/com/trs/Runner.java
@@ -1,7 +1,24 @@
 package com.trs;
 
-public class Runner {
-    public static void main(String[] args) {
+import com.trs.controllers.FormNavigator;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Runner extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent loginParent = FXMLLoader.load(Objects.requireNonNull(FormNavigator.class.getResource("/com/trs/forms/Login.fxml")));
+        Scene loginScene = new Scene(loginParent);;
+        stage.setScene(loginScene);
+        stage.show();
 
     }
 }
